0bfe697597efc3e98eebac0fe189d7cfe24b686a
ArchitTheCoder <72322982+ArchitTheCoder@users.noreply.github.com>
2021-05-11 14:41:27 -0700

    Add files via upload

---
+++ Removefiles.py
@@ -0,0 +1,58 @@
+import time
+import os
+import shutil
+
+def main():
+    deleted_folders_count = 0
+    deleted_files_count = 0
+
+    path = ""
+
+    days = 30
+    seconds = time.time() - (days * 24 * 60 * 60)
+    if os.path.exists(path):
+         for root_folder, folders, files in os.walk(path):
+            if seconds >= get_file_or_folder_age(root_folder):
+                remove_folder(root_folder)
+                deleted_folder_count += 1
+                break
+            else:
+                for folder in folders:
+                    folder_path = os.path.join(root_folder, folder)
+                    if seconds >= get_file_or_folder_age(folder_path):
+                        remove_folder(folder_path)
+                        deleted_folder_count += 1
+                for file in files:
+                    file_path = os.path.join(root_folder, file)
+                    if seconds >= get_file_or_folder_age(file_path):
+                        remove_file(file_path)
+                        deleted_file_count += 1
+                    else:
+                        if seconds >= get_file_or_folder_age(path):
+                            remove_file(path)
+                            deleted_file_count += 1
+    else:
+        print(f"Total folders deleted {deleted_folders_count}")
+        print(f"Total files deleted {deleted_files_count}")
+
+
+def remove_folder(path):
+    if not shutil.rmtree(path):
+        print(f"{path} was removed successfully")
+    else:
+        print("Unable to delete path")
+
+
+def remove_file(path):
+    if not os.remove(path):
+        print(f"{path} was removed successfully")
+    else:
+        print("Unable to delete path")
+
+
+
+def get_file_or_folder_age(folder_path):
+    ctime = os.stat(folder_path).st_ctime 
+    return ctime
+
+main()
